ce7c2d5be09bda6b5ffeb8bb5d1a6a0df620cf4b
Tamerlan Swift <majn39143@gmail.com>
2025-08-08 15:17:33 +0500

    feat(Home): Added search bar Added new View struct -SearchBarView A coin filtering logic has been created

---
+++ CoinTrackerApp/Core/Components/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  CoinTrackerApp
+//
+//  Created by Tamerlan Swift on 07.08.2025.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
